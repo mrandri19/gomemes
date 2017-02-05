4e0b055223f6b96bd53881c327e6b2713429450d
mrandri19 <andrecogno@hotmail.it>
2017-02-05 14:48:43 +0100

    Added caching

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request
+from werkzeug.contrib.cache import SimpleCache
 
 from PIL import ImageFont, Image, ImageDraw
 
 import base64
 from io import BytesIO
 
-
-img = Image.open("go_template.jpg")
-
 def write_on_image(text):
+    img = Image.open("go_template.jpg")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("Go-Mono.ttf", 20)
 
@@ -22,15 +21,18 @@ def write_on_image(text):
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    if request.method == 'GET':
-        return render_template('index.html')
+cache = SimpleCache()
 
-    text = request.form['text']
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/image")
+def image():
+    text = request.args.get("text", "")
     if text == "" or text == None:
         return 'Text missing from form data', 400
-
+        
     # Split the string in lists <= 32 chars
     text_tmp = []
     line_tmp = ""
@@ -49,10 +51,19 @@ def hello():
     counter = 0
 
     # Join them with a newline
+    text_tmp = map((lambda s: s.strip()), text_tmp)
     text = "\n".join(text_tmp)
     # 8x32 monospace go-mono.ttf with size 20
 
+    if cache.get(text):
+       print("Used cached value")
+       return render_template('image.html', data=cache.get(text)) 
+
     image_str = write_on_image(text).decode()
+
+    print("added to cache")
+    cache.set(text, image_str)
+
     return render_template('image.html', data=image_str)
 
 if __name__ == "__main__":
